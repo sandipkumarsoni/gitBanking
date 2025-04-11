19265ed07dbe9d4590d302d4fc8b93f7641b7400
SandipSoni <skk.soni@gmail.com>
2025-04-11 20:45:15 +0530

    py file added

---
+++ sample.py
@@ -0,0 +1,13 @@
+# Sample Python Code
+
+def add_numbers(a, b):
+    return a + b
+
+# Print a greeting
+print("Hello, World!")
+a = 200
+b = 300
+
+# Call the function
+result = add_numbers(5, 7)
+print("The sum is:", result)
