15abac76b56ae20d9b74e65514a60d0717a73c0e
SandipSoni <skk.soni@gmail.com>
2025-04-11 21:17:37 +0530

    Saaaa

--- sample.py
+++ sample.py
@@ -1,13 +1,14 @@
 # Sample Python Code
-
 def add_numbers(a, b):
     return a + b
 
 # Print a greeting
+import pdb; pdb.set_trace()  # Set a breakpoint here
 print("Hello, World!")
 a = 200
 b = 300
-
+import pdb; pdb.set_trace()  # Set a breakpoint here
 # Call the function
 result = add_numbers(5, 7)
+print("Sandip soni")
 print("The sum is:", result)
